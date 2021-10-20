11c16dcda720d9f9cc5b165670afcaaec7e8785b
maucoderGit <dreg5000.bbb@gmail.com>
2021-10-20 16:45:04 -0400

    adding new characters

--- index.js
+++ index.js
@@ -6,6 +6,31 @@ app.get("/", (req, res) => {
   res.send("Hello! I'm your new e-commerce :D");
 })
 
+app.get("/new-rute", (req,res) => {
+  res.send("Hello, I'm a new rute")
+})
+
+app.get("/products", (req,res) => {
+  res.json({
+    categorie: "character",
+    name: "Monkey D. Luffy",
+    phrase: "I'll become pirate king"
+  },
+  {
+    categorie: "character",
+    name: "Nami",
+    phrase: "I'll make the world map"
+  },
+  {
+    categorie: "character",
+    name: "Vizmoke Sanji",
+    phrase: "I'll find the All Blue"
+  }
+  )
+})
+
 app.listen(port,() => {
   console.log("My port" + port)
 });
+
+// Install JSON Viewer as navigator extention
