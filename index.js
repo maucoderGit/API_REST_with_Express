1c19611fa438c4449f07f540390e813e0a636ce3
maucoderGit <dreg5000.bbb@gmail.com>
2021-10-20 18:17:08 -0400

    adding new app.get

--- index.js
+++ index.js
@@ -4,33 +4,58 @@ const port = 3000;
 
 app.get("/", (req, res) => {
   res.send("Hello! I'm your new e-commerce :D");
-})
+});
 
 app.get("/new-rute", (req,res) => {
-  res.send("Hello, I'm a new rute")
-})
+  res.send("Hello, I'm a new rute");
+});
 
-app.get("/products", (req,res) => {
-  res.json({
+app.get("/characters", (req,res) => {
+  res.json([{
     categorie: "character",
     name: "Monkey D. Luffy",
     phrase: "I'll become pirate king"
-  },
-  {
+  }, {
+
     categorie: "character",
     name: "Nami",
     phrase: "I'll make the world map"
-  },
-  {
+  }, {
     categorie: "character",
     name: "Vizmoke Sanji",
     phrase: "I'll find the All Blue"
-  }
-  )
+  }]);
+});
+
+const characters_phrases = "/characters/:id/phrase/:phraseId";
+app.get(characters_phrases, (req,res) => {
+  const { id, phraseId } = req.params
+  res.json({
+    id,
+    phraseId
+  });
+});
+
+const family = "/characters/:id/phrase/:phraseId";
+app.get(family, (req,res) => {
+  const { id, phraseId } = req.params
+  res.json({
+    id,
+    phraseId
+  });
+});
+
+
+app.get("/categories/:categoryId/characters/:id", (req,res) => {
+  const {categoryId, id} = req.params;
+  res.json({
+    categoryId,
+    id
+  })
 })
 
 app.listen(port,() => {
-  console.log("My port" + port)
+  console.log("My port" + port);
 });
 
 // Install JSON Viewer as navigator extention
