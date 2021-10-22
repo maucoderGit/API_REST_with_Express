6b33ff2cc0628639fbf90fd0bb0b3e73c0ecc300
maucoderGit <dreg5000.bbb@gmail.com>
2021-10-22 18:33:15 -0400

    Moduling routes

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const faker = require("faker");
 const express = require("express");
-const { image } = require("faker");
+const routerAPI = require("./src/index");
+
 const app = express();
 const port = 3000;
 
@@ -12,78 +12,14 @@ app.get("/new-rute", (req,res) => {
   res.send("Hello, I'm a new rute");
 });
 
-app.get("/characters/filter", (req, res) => {
-  res.send("Yo soy un filter");
-});
+// app.get("/categories/:categoryId/characters/:id", (req,res) => {
+//   const {categoryId, id} = req.params;
+//   res.json({
+//     categoryId,
+//     id
+//   })
 
-app.get("/characters", (req,res) => {
-  res.json([{
-    categorie: "character",
-    name: "Monkey D. Luffy",
-    phrase: "I'll become pirate king",
-    reward: 1500000
-  }, {
-    categorie: "character",
-    name: "Nami",
-    phrase: "I'll make the world map"
-  }, {
-    categorie: "character",
-    name: "Vizmoke Sanji",
-    phrase: "I'll find the All Blue"
-  }]);
-});
-
-const characters_phrases = "/characters/:id";
-app.get(characters_phrases, (req,res) => {
-  const { id, phraseId } = req.params
-  res.json({
-    id
-  });
-});
-
-const family = "/characters/:id/phrase/:phraseId";
-app.get(family, (req,res) => {
-  const { id, phraseId } = req.params
-  res.json({
-    id,
-    phraseId
-  });
-});
-
-app.get("/categories/:categoryId/characters/:id", (req,res) => {
-  const {categoryId, id} = req.params;
-  res.json({
-    categoryId,
-    id
-  })
-})
-
-app.get("/users", (req, res) => {
-  const { limit, offset } = req.query;
-  if (limit && offset) {
-    res.json({
-      limit,
-      offset
-    })
-  } else {
-    res.send("Have not parameters")
-  }
-})
-
-
-app.get("/products", (req, res) => {
-  const products = [];
-  const { size } = req.query;
-  const limit = size || 10;
-  for(i = 0; i < limit; i++){
-    products.push({
-      name: faker.commerce.productName(),
-      price: parseInt(faker.commerce.price(), 10),
-      img: faker.image.imageUrl(),
-    })
-  };
-  res.json(products);
-});
+routerAPI(app)
 
 app.listen(port,() => {
   console.log("My port" + port);
