660c626c84745aaaef853768488dbcad69ecae57
maucoderGit <dreg5000.bbb@gmail.com>
2021-10-26 16:10:43 -0400

    class services

--- services/productsServices.js
+++ services/productsServices.js
@@ -7,15 +7,15 @@ class ProductsService {
     this.generate();
   }
 
-  generate(){
+  generate() {
     const limit = 100;
-    for(let i = 0; i < limit; i++){
-      this.products.push([{
-        id: faker.datatype.uuid,
+    for (let i = 0; i < limit; i++) {
+      this.products.push({
+        id: faker.datatype.uuid(),
         name: faker.commerce.productName(),
         price: parseInt(faker.commerce.price(), 10),
-        img: faker.image.imageUrl(),
-      }]);
+        image: faker.image.imageUrl(),
+      });
     }
   }
 
@@ -23,12 +23,12 @@ class ProductsService {
 
   }
 
-  find(id){
-    return this.products.find(item => item.id === id);
+  find(){
+    return this.products.find;
   }
 
-  findOne(){
-
+  findOne(id){
+    return this.products.find(item => item.id === id);
   }
 
   update(){
